5a08b215d5cbbaad9a86e3a80278ab856e02f3dc
meiwen520gmx <1045418346@qq.com>
2020-09-23 16:58:33 +0800

    补充说明代理

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -14,3 +14,9 @@ module.exports = function (app) {
   //   changeOrigin: true
   // }))
 };
+
+/**
+ * 1.匹配到devApi,开始做代理去替换这个地址： http://www.web-jshtml.cn/api/react，请求地址就会变成/devApi/login/
+ * 2.pathRewrite 主要是把/devApi/login/ =>变成 /login/
+ * 3.替换之后的地址： http://www.web-jshtml.cn/api/react/login
+ */
